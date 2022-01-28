c8f2122c9ed0063a72f4ab0247a3c7dcd1df64c5
vireen2124 <24vireen@gmail.com>
2022-01-28 02:43:58 +0000

    added extra media queries and a cross

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const burgerDiv=document.querySelector(".burger-div");
 const navLinks=document.querySelector(".nav-links");
 const socialMediaLinks=document.querySelector(".nav-social-media");
+const closeCircle=document.querySelector(".close-circle")
 burgerDiv.addEventListener("click",()=>{
     navLinks.classList.toggle("nav-is-active");
     socialMediaLinks.classList.toggle("nav-is-active");
     burgerDiv.classList.toggle("burger-clicked")
+})
+closeCircle.addEventListener("click",()=>{
+    navLinks.classList.remove("nav-is-active");
+    socialMediaLinks.classList.remove("nav-is-active");
+    burgerDiv?.classList.remove("burger-clicked")
 })
